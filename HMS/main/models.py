ebf41dadf7c7fa072791ba65f4730339a21bff1b
Krish Khimasia <khimasiakrish@gmail.com>
2023-04-03 15:35:22 +0530

    hall budget, allocs, exps

--- HMS/main/models.py
+++ HMS/main/models.py
@@ -11,3 +11,44 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
+
+class Allocation(models.Model):
+    name = models.CharField(max_length = 100)
+    allocated_grant = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+    def change_value(self, value):
+        self.allocated_grant = value
+        self.save()
+
+class Expense(models.Model):
+    name = models.CharField(max_length = 100)
+    cost = models.FloatField()
+    
+    def __str__(self):
+        return self.name
+   
+    def change_value(self, value):
+        self.cost = value
+        self.save()
+
+class HallBudget(models.Model):
+    hall = models.OneToOneField(Hall, on_delete = models.CASCADE, related_name = "hallBudget")
+    expenses = models.ForeignKey(Expense, on_delete=models.PROTECT)     # PROTECT raises ProtectedError when Expense object is deleted
+    pettyexpenses = models.ForeignKey(Expense, on_delete=models.PROTECT)
+    allocations = models.ForeignKey(Allocation, on_delete=models.PROTECT)
+    #hallPhoto = models.ImageField()
+    
+    def __str__(self):
+        return self.allocations #dont know what to return here
+    
+    def get_total(self):
+        return - self.expenses - self.pettyexpenses + self.allocations
+    
+    def get_petty_expenses(self):
+        return self.pettyexpenses
+    
+    def get_allocations(self):
+        return self.allocations
